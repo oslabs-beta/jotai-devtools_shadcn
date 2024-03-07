f3198fdd979335cd8027f0fa6a0ab45703ab7c23
tori-obregon <vobregon@gmail.com>
2024-03-07 11:36:20 -0800

    Revert "custom jotai dev tool"
    This reverts commit d0d3fe8f1ae04d253eabf670ac41ee85f5f6e71c.

--- JotaiDevTool/src/DevTools/hooks/useThemeMode.tsx
+++
@@ -1,6 +0,0 @@
-import { useMantineColorScheme } from '@mantine/core';
-
-export const useThemeMode = <L, T>(light: L, dark: T) => {
-  const { colorScheme } = useMantineColorScheme();
-  return colorScheme === 'light' ? light : dark;
-};
