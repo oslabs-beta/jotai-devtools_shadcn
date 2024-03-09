8f25b8bff605880c31da8faf96bf9c589f4eacf1
tori-obregon <vobregon@gmail.com>
2024-03-09 10:26:33 -0800

    committiing save

--- JotaiDevTool/src/DevTools/Extension/components/Shell/components/AtomGraph/Graph.tsx
+++ JotaiDevTool/src/DevTools/Extension/components/Shell/components/AtomGraph/Graph.tsx
@@ -14,8 +14,6 @@ const nodeTypes = {
   custom: CustomNode,
 };
 
-// const nodeAtoms = 
-
 const initialNodes = [
   { id: '1', type: 'custom', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', type: 'custom', position: { x: 0, y: 100 }, data: { label: '2' } },
