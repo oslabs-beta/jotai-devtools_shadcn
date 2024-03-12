98ab49226e28b852531c6b7c5316b7cb1ae486b2
tori-obregon <vobregon@gmail.com>
2024-03-11 19:08:56 -0700

    ability to retrieve all atoms as an object for atom graph complete

--- JotaiDevTool/src/DevTools/Extension/components/Shell/components/AtomGraph/Graph.tsx
+++ JotaiDevTool/src/DevTools/Extension/components/Shell/components/AtomGraph/Graph.tsx
@@ -9,6 +9,12 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 import 'reactflow/dist/base.css';
 import CustomNode from './CustomNode';
+import { useAtomValue, useAtom } from 'jotai/react';
+import { valuesAtom } from '../../../../../atoms/values-atom';
+import { atomWithDefault } from 'jotai/vanilla/utils';
+import { ValuesAtomTuple } from 'src/types';
+import { useDevtoolsJotaiStoreOptions } from '../../../../../internal-jotai-store';
+import { useSyncSnapshotValuesToAtom } from '../../../../../hooks/useAtomsSnapshots';
 
 const nodeTypes = {
   custom: CustomNode,
@@ -20,13 +26,33 @@ const initialNodes = [
   { id: '3', type: 'custom', position: { x: 0, y: 200 }, data: { label: '3' } },
 ];
 
-
 const initialEdges = [
   // { id: 'e1-2', source: '1', target: '2' },
   // { id: 'el1-3', source:'1', target: '3'}
 ];
 
+const allValues = atomWithDefault<ValuesAtomTuple[]>((get) => {
+  const allAtoms = get(valuesAtom);
+  return allAtoms;
+});
+
 function Reactflow() {
+  useSyncSnapshotValuesToAtom();
+  
+  const values = useAtomValue(
+    allValues,
+    useDevtoolsJotaiStoreOptions(),
+  )
+
+  console.log('values', values);
+  
+  const valuesRef = React.useRef(values);
+  
+  React.useEffect(() => {
+    valuesRef.current = values;
+  }, [values]);
+  
+  
   const [ nodes, setNodes, onNodesChange ] = useNodesState(initialNodes);
   const [ edges, setEdges, onEdgesChange ] = useEdgesState(initialEdges);
 
@@ -34,25 +60,25 @@ function Reactflow() {
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
-
-  return (
-      <div style={{ width: '90%', height: '90%' }}>
-        <ReactFlow
-          fitView
-          nodes={nodes}
-          nodeTypes={nodeTypes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          style={{ backgroundImage: "linear-gradient(to bottom, #758696, #252B37)"  }}
-
-        >
-          <Controls />
-          <Background color="#252B37" variant='lines' />
-        </ReactFlow>
-      </div>
-  );
+    
+      return (
+          <div style={{ width: '90%', height: '90%' }}>
+            <ReactFlow
+              fitView
+              nodes={nodes}
+              nodeTypes={nodeTypes}
+              edges={edges}
+              onNodesChange={onNodesChange}
+              onEdgesChange={onEdgesChange}
+              onConnect={onConnect}
+              style={{ backgroundImage: "linear-gradient(to bottom, #758696, #252B37)"  }}
+    
+            >
+              <Controls />
+              <Background color="#252B37" variant='lines' />
+            </ReactFlow>
+          </div>
+      );
 }
 
 export default Reactflow;
